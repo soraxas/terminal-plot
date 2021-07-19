bea3dcdd0e4b5fd1d7c137a66212e6743db2319d
Tin Lai <oscar@tinyiu.com>
2021-07-20 01:26:34 +1000

    --amend
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tensorboard-termplot",
-    version="1.1.1",
+    version="1.1.2",
     description="View tensorboard stats inside terminal.",
     long_description=long_description,
     long_description_content_type="text/markdown",
