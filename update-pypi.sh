8c3ffbe3a611147d3a1db8863a8a063d184e52d5
Tin Lai <oscar@tinyiu.com>
2021-07-20 11:22:28 +1000

    auto remove old dist
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- update-pypi.sh
+++ update-pypi.sh
@@ -4,5 +4,8 @@ python=/usr/bin/python
 
 echo "Make sure you had updated the version number in setup.py"
 
+rm -rf dist/
+rm -rf *.egg-info/
+
 $python -m build
 $python -m twine upload --repository pypi dist/*
